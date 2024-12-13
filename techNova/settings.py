4ab94a91b4f880b578508f9af7cf6946d1d4300e
MrSaucer <108151584+MrSaucer@users.noreply.github.com>
2024-12-13 02:25:32 -0300

    Update settings.py

--- techNova/settings.py
+++ techNova/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'", "'unsafe-eval'")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
